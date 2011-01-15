2f0478abcf6d7cf3763c514edd9a5f235b1e77b6
Tiago Augusto Sciencia <tisciencia@gmail.com>
2011-01-14 23:20:33 -0200

    Create project structure

---
+++ src/AnyDoubts.Domain/Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyDoubts.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+    }
+}
