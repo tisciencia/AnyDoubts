97d410b228d18da76efb8fecfdebafcad050f5cd
Tiago Augusto Sciencia <tisciencia@gmail.com>
2011-01-16 16:42:40 -0200

    Implemented the Question class

---
+++ src/AnyDoubts.Domain/Entities/Question.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyDoubts.Domain.Entities
+{
+    public class Question
+    {
+        private const int MESSAGE_MAX_LENGTH = 255;
+        private string _message;
+        private string _answer;
+
+        public string Message
+        {
+            get { return _message; }            
+        }
+        public string Answer
+        {
+            get { return _answer; }
+            set { _answer = value; }
+        }
+        public bool IsAnswered
+        {
+            get 
+            {
+                if (String.IsNullOrEmpty(_answer))
+                    return false;
+                else
+                    return true;
+            }
+        }
+        
+        private Question() { }
+        public Question(string message)
+        {
+            if (IsMessageValid(message))
+                this._message = message;
+            else
+                throw new ArgumentException("Iinvalid message");
+        }
+
+        public bool IsMessageValid(string message)
+        {
+            if ((String.IsNullOrEmpty(message)) || (message.Trim().Length > MESSAGE_MAX_LENGTH))
+                return false;
+            else 
+                return true;
+        }        
+    }
+}
---
+++ src/AnyDoubts.Tests/Domain/QuestionTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SharpTestsEx;
+using AnyDoubts.Domain.Entities;
+
+namespace AnyDoubts.Tests.Domain
+{
+    [TestFixture]
+    public class QuestionTests
+    {
+        public const int MESSAGE_MAX_LENGTH = 255;
+
+        [Test]
+        public void QuestionMessageShouldBeValid()
+        {
+            Question question = new Question("Olá tudo bem?");
+            question.Message.Should().Be("Olá tudo bem?");
+        }
+
+        [Test]
+        [ExpectedException("System.ArgumentException")]
+        public void QuestionMessageShouldBeInvalidWhenEmpty()
+        {
+            Question question = new Question("");      
+        }
+
+        [Test]
+        [ExpectedException("System.ArgumentException")]
+        public void QuestionMessageShouldBeInvalidWhenNull()
+        {
+            Question question = new Question(null);
+        }
+
+        [Test]        
+        public void QuestionMessageShouldBeValidWithMessage255()
+        {
+            char[] charArrayMaxLength = new char[MESSAGE_MAX_LENGTH];
+            for (int i = 0; i < MESSAGE_MAX_LENGTH; i++)
+                charArrayMaxLength[i] = '.';
+            
+            string messageMaxLength = new string(charArrayMaxLength);
+            Question question = new Question(messageMaxLength);
+
+            question.Message.Length.Should().Be(MESSAGE_MAX_LENGTH);
+        }
+
+        [Test]
+        [ExpectedException("System.ArgumentException")]
+        public void QuestionMessageShouldBeInvalidWithMessage256()
+        {
+            char[] charArrayOverMaxLength = new char[MESSAGE_MAX_LENGTH + 1];
+            for (int i = 0; i < MESSAGE_MAX_LENGTH + 1; i++)
+                charArrayOverMaxLength[i] = '.';
+
+            string messageOverMaxLength = new string(charArrayOverMaxLength);
+            Question question = new Question(messageOverMaxLength);
+        }
+
+        [Test]
+        public void QuestionAnswerShouldBeAnswered()
+        {
+            Question question = new Question("Olá tudo bem?");
+            question.Answer = "Sim obrigado.";
+            
+            question.IsAnswered.Should().Be(true);
+        }
+
+        [Test]
+        public void QuestionAnswerShouldBeNotAnswered()
+        {
+            Question question = new Question("Olá tudo bem?");
+            
+            question.IsAnswered.Should().Be(false);
+        }
+    }
+}
