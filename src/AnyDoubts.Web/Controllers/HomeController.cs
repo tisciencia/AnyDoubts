a7ef74c9087bc86c73527200a9a90232ed64014f
Tiago Augusto Sciencia <tisciencia@gmail.com>
2011-01-19 21:51:46 -0200

    Implements part of views [home, sobre, colaboradores] and controllers [home]

--- src/AnyDoubts.Web/Controllers/HomeController.cs
+++ src/AnyDoubts.Web/Controllers/HomeController.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AnyDoubts.Domain.Model;
 
 namespace AnyDoubts.Web.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {            
+            ViewBag.Message = "Ask me anything";
+
+            List<Question> listaPerguntas = new List<Question>();
+            Question q1 = new Question("Pergunta 1");
+            q1.Answer = "Resposta 1";
+            listaPerguntas.Add(q1);
+            Question q2 = new Question("Pergunta 2");
+            q2.Answer = "Resposta 2";
+            listaPerguntas.Add(q2);
+
+            return View(listaPerguntas);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(string question)
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            Question newQuestion = new Question(question);
+
+            //newQuestion.Message;
 
             return View();
         }
 
-        public ActionResult About()
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        public ActionResult Colaboradores()
         {
             return View();
         }
