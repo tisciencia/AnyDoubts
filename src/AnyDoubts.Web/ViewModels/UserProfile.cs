976984564b1df01b3a5d6e112711b1a468b4bc56
Tiago Augusto Sciencia <tisciencia@gmail.com>
2011-02-23 22:50:42 -0300

    somes refactorings :P

---
+++ src/AnyDoubts.Web/ViewModels/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AnyDoubts.Domain.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnyDoubts.Web.ViewModels
+{
+    public class UserProfile
+    {
+        public IList<Question> Questions { get; set; }
+        
+        [Required]
+        [Display(Name = "Pergunte-me o que quiser")]        
+        [StringLength(255, ErrorMessage = "Must be less than 255 characters")]
+        public string NewQuestion { get; set; }
+    }
+}
