e3a4db82e3549ac0161274e6d7987d359832164c
Tiago Sciencia <tisciencia@gmail.com>
2011-02-25 17:16:09 -0300

    implemented delete questions

---
+++ src/AnyDoubts.Web/ViewModels/Inbox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AnyDoubts.Domain.Model;
+
+namespace AnyDoubts.Web.ViewModels
+{
+    public class Inbox
+    {
+        public IList<Question> Questions { get; set; }
+    }
+}
